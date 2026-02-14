f615726c5da3c04aa69af470f8621680a8196d1d
Prashanrt Bhatt <prashantbhatt488@gmail.com>
2026-02-14 09:26:19 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/newOne/',   // 👈 required for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
